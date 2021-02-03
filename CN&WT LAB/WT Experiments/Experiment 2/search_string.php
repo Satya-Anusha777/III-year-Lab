14b322bdcdbdc8ad32c58f5636d7a99434f5c797
Satya-Anusha <52241090+Satya-Anusha@users.noreply.github.com>
2021-02-03 16:01:43 +0530

    Add files via upload

---
+++ CN&WT LAB/WT Experiments/Experiment 2/search_string.php
@@ -0,0 +1,18 @@
+
+<?php
+
+$str1 = 'Welcome to WT Lab';
+if (strpos($str1,'Lab') !== false) 
+ {
+    echo 'The specific word is present.';
+ }
+else
+ {
+    echo 'The specific word is not present.';
+ }
+
+?>
+ 
+ Output:
+
+The specific word is present.
---
+++ CN&WT LAB/WT Experiments/Experiment 2/string_reverse.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$string = "Anusha";  
+echo "Reverse string of $string is " .strrev ( $string );  
+
+?>
+
+
+
+Output:
+
+Reverse string of Anusha is ahsunA
